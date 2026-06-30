2172816bf2b683e7bc1bd9f87e8c937f0dcd63bd
Omkar Deshmukh <138182078+omkardeshmukh123@users.noreply.github.com>
2026-06-30 13:42:33 +0530

    fix

--- Module 4 NUnit and Moq Complete/CustomerCommSolution/CustomerCommLib/obj/Debug/net10.0/CustomerCommLib.AssemblyInfo.cs
+++ Module 4 NUnit and Moq Complete/CustomerCommSolution/CustomerCommLib/obj/Debug/net10.0/CustomerCommLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerCommLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e866342562a7e8ef20ea27c6c558705f05b72e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edc0079b0b7fd8efc3cd5f228ad22365dfd0ba58")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerCommLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerCommLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
